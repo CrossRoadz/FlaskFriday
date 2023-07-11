ca18aa521a2bbab6d8952d9a2bba1e24a10d5680
Jacob Wiggers <jacobwiggers@outlook.com>
2023-07-11 15:04:30 -0400

    templates/base.html

--- hello.py
+++ hello.py
@@ -1,20 +1,45 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 #flask instance
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "qqqQQQqqq"
+
+#create a form class
+class NamerForm(FlaskForm):
+	name = StringField("Input your name here", validators=[DataRequired()])
+	submit = SubmitField("Submit")
+
+
 
 #create route decorator
 @app.route("/")
 def index():
-	stuff = "this is bold text"
-	pizza = ["Pepperoni", "Cheese","Mushroom",41]
-	return render_template("index.html",
-		stuff = stuff,
-		pizza=pizza)
+	return render_template("index.html")
 
 @app.route("/user/<name>")
 def user(name):
 	return render_template("user.html",name=name)
+
+
+@app.route("/name", methods=['GET', 'POST'])
+def name():
+
+
+	name = None
+	form = NamerForm()
+
+	#validate form
+	if form.validate_on_submit():
+		name = form.name.data
+		form.name.data = ""
+		flash("Name Submitted Successfully!")
+
+	return render_template('name.html',
+		name=name,
+		form=form)
 
 #custom error pages
 
@@ -27,3 +52,6 @@ def page_not_found(e):
 @app.errorhandler(500)
 def page_not_found(e):
 	return render_template("500.html"), 500
+
+#create a form class
+
